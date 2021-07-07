b6a0b8c8f35a5bfc603653550faec4734c7bd134
M Manzur Morshed <mmorshed@Ms-MacBook-Pro.local>
2021-07-07 16:11:44 -0400

    added sample code..refactored..added own realtime database

---
+++ app/src/main/java/edu/neu/madcourse/numad21sugroupattentionstick/notification/ReceiveNotificationActivity.java
@@ -0,0 +1,16 @@
+package edu.neu.madcourse.numad21sugroupattentionstick.notification;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.neu.madcourse.numad21sugroupattentionstick.R;
+
+public class ReceiveNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_notification);
+    }
+}
