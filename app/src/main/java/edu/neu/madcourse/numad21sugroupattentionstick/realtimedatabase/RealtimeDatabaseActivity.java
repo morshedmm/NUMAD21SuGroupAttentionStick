d97f9cdd09487806ec5bc6b4648e36974e3f6a0c
ColeStansbury <stansbury.c@northeastern.edu>
2021-07-11 21:22:37 -0400

    Clean up

--- app/src/main/java/edu/neu/madcourse/numad21sugroupattentionstick/realtimedatabase/RealtimeDatabaseActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21sugroupattentionstick/realtimedatabase/RealtimeDatabaseActivity.java
@@ -64,13 +64,8 @@ public class RealtimeDatabaseActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_realtime_database);
-
-    //user1 = (TextView) findViewById(R.id.username1);
-    //user2 = (TextView) findViewById(R.id.username2);
-    //score_user1 = (TextView) findViewById(R.id.score1);
-    //score_user2 = (TextView) findViewById(R.id.score2);
-    player = (RadioButton) findViewById(R.id.my_stickers);
-    radioGroup = (RadioGroup) findViewById(R.id.radioGroup1);
+    player = findViewById(R.id.my_stickers);
+    radioGroup = findViewById(R.id.radioGroup1);
 
     //
     radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
@@ -91,10 +86,6 @@ public class RealtimeDatabaseActivity extends AppCompatActivity {
         }
       }
     });
-
-
-    //
-
     // Connect with firebase
     //
     mDatabase = FirebaseDatabase.getInstance().getReference();
@@ -102,7 +93,6 @@ public class RealtimeDatabaseActivity extends AppCompatActivity {
     // Update the score in realtime
     mDatabase.child("users").addChildEventListener(
             new ChildEventListener() {
-
               @Override
               public void onChildAdded(DataSnapshot dataSnapshot, String s) {
                 drawEmotes(dataSnapshot);
@@ -117,12 +107,10 @@ public class RealtimeDatabaseActivity extends AppCompatActivity {
 
               @Override
               public void onChildRemoved(DataSnapshot dataSnapshot) {
-
               }
 
               @Override
               public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
               }
 
               @Override
@@ -135,14 +123,28 @@ public class RealtimeDatabaseActivity extends AppCompatActivity {
     );
   }
 
-  public void setUserName(View view) {
+  private void createRecyclerView() {
+    rLayoutManger = new LinearLayoutManager(this);
 
-        /*
-        String curName = findViewById(R.id.myusername_id).toString();
-        User myUser = new User(curName, "0");
-        Task t3 = mDatabase.child("users").child(myUser.username).setValue(myUser);
-        */
+    recyclerView = findViewById(R.id.recycler_view2);
+    recyclerView.setHasFixedSize(true);
+    //recyclerView.setNestedScrollingEnabled(false);
 
+    rviewAdapter = new MyRviewAdapter(itemList);
+
+    recyclerView.setAdapter(rviewAdapter);
+    recyclerView.setLayoutManager(rLayoutManger);
+  }
+
+  public void addImage1(View view) {
+    addEmote(view, "1");
+  }
+
+  public void addImage2(View view) {
+    addEmote(view, "2");
+  }
+
+  public void login(View view) {
     try {
       EditText givenName = (EditText) findViewById(R.id.myusername_id);
       String curName = givenName.getText().toString();
@@ -168,24 +170,14 @@ public class RealtimeDatabaseActivity extends AppCompatActivity {
       Toast.makeText(RealtimeDatabaseActivity.this,
                      "Login failed!", Toast.LENGTH_SHORT).show();
     }
-
-
-  }
-
-  public void addImage1(View view) {
-    addEmote(view, "1");
-  }
-
-  public void addImage2(View view) {
-    addEmote(view, "2");
   }
 
   // Add Emote based on String
   public void addEmote(View view, String imageNum) {
 
-    EditText receiverUsernameField = (EditText) findViewById(R.id.receiverusername_id);
+    EditText receiverUsernameField = findViewById(R.id.receiverusername_id);
     String receiverUsername = receiverUsernameField.getText().toString();
-    if (receiverUsername == null || receiverUsername.equals("")) {
+    if (receiverUsername.equals("")) {
       Log.i("Adding Emote", "No receiver username entered!");
       Toast.makeText(RealtimeDatabaseActivity.this,
                      "No receiver username entered!", Toast.LENGTH_SHORT).show();
@@ -197,29 +189,159 @@ public class RealtimeDatabaseActivity extends AppCompatActivity {
                      "Please log in before sending emotes!", Toast.LENGTH_SHORT).show();
       return;
     }
-
-    //User receiver = new User(curName, "0", "");
-
-    //RealtimeDatabaseActivity.this.onAddScore(mDatabase, player.isChecked() ? "user1" : "user2",
-    // imageNum);
     RealtimeDatabaseActivity.this.onAddScore(mDatabase, receiverUsername, imageNum,
                                              myUser.username);
     RealtimeDatabaseActivity.this.onAddScore(mDatabase, myUser.username + "sent", imageNum,
                                              receiverUsername);
   }
 
-  private void createRecyclerView() {
-    rLayoutManger = new LinearLayoutManager(this);
 
-    recyclerView = findViewById(R.id.recycler_view2);
-    recyclerView.setHasFixedSize(true);
-    //recyclerView.setNestedScrollingEnabled(false);
+  /**
+   * Called on score_user1 add
+   *
+   * @param postRef
+   * @param user
+   */
+  private void onAddScore(DatabaseReference postRef, String user, String imageNum,
+                          String senderName) {
+    postRef
+            .child("users")
+            .child(user)
+            .runTransaction(new Transaction.Handler() {
+              @Override
+              public Transaction.Result doTransaction(MutableData mutableData) {
 
-    rviewAdapter = new MyRviewAdapter(itemList);
+                User user = mutableData.getValue(User.class);
+                if (user == null) {
+                  return Transaction.success(mutableData);
+                }
+                user.score = user.score + imageNum + ",";
+                user.senders = user.senders + senderName + ",";
+                user.scoreTimes = user.scoreTimes + Utils.date() + ",";
+                mutableData.setValue(user);
+                return Transaction.success(mutableData);
+              }
 
-    recyclerView.setAdapter(rviewAdapter);
-    recyclerView.setLayoutManager(rLayoutManger);
+              @Override
+              public void onComplete(DatabaseError databaseError, boolean b,
+                                     DataSnapshot dataSnapshot) {
+                // Transaction completed
+                Log.d(TAG, "postTransaction:onComplete:" + databaseError);
+                Toast.makeText(getApplicationContext(), "DBError: " + databaseError,
+                               Toast.LENGTH_SHORT).show();
+              }
+            });
+  }
 
+  private String parseData(String inputStr) throws JSONException {
+    JSONObject jObject = new JSONObject(inputStr);
+    Iterator<?> keys = jObject.keys();
+    while (keys.hasNext()) {
+      String key = (String) keys.next();
+      if (key.equals(USER_1)) {
+        return jObject.getString(key);
+      }
+    }
+    return "Nothing matched!";
+
+  }
+
+  public void showReadData(View view, String addition) {
+    mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        String curUsername = myUser.username;
+
+        String[] stickerIdList = getStringList(String.valueOf(dataSnapshot.child("users")
+                                                                      .child(curUsername + addition).child("score").getValue()));
+        String[] userList = getStringList(String.valueOf(dataSnapshot.child("users")
+                                                                 .child(curUsername + addition).child("senders").getValue()));
+        String[] msgTimeList = getStringList(String.valueOf(dataSnapshot.child("users")
+                                                                    .child(curUsername + addition).child("scoreTimes").getValue()));
+        itemList = new ArrayList<>();
+        for (int idx = 0; idx < stickerIdList.length; idx++) {
+          if (stickerIdList[idx].equals("1")) {
+            MyItemCard itemCard = new MyItemCard(R.drawable.foo,
+                                                 userList[idx], msgTimeList[idx], false);
+            itemList.add(itemCard);
+            //continue;
+          } else if (stickerIdList[idx].equals("2")) {
+            MyItemCard itemCard = new MyItemCard(R.drawable.thinking_face, "",
+                                                 userList[idx], false);
+            itemList.add(itemCard);
+          }
+        }
+        createRecyclerView();
+      }
+
+      @Override
+      public void onCancelled(DatabaseError databaseError) {
+      }
+    });
+  }
+
+  private String[] getStringList(String inputString) {
+    return inputString.split(",");
+  }
+
+
+  private void drawEmotes(DataSnapshot dataSnapshot) {
+    User user = dataSnapshot.getValue(User.class);
+
+    String whoseInfo = player.isChecked() ? myUser.username : myUser.username + "sent";
+    Log.i("INFO", whoseInfo);
+
+    if (dataSnapshot.getKey().equalsIgnoreCase(whoseInfo)) {
+      String[] stickerIdList = getStringList(String.valueOf(user.score));
+      String[] userList = getStringList(String.valueOf(user.senders));
+      itemList = new ArrayList<>();
+      for (int idx = 0; idx < stickerIdList.length; idx++) {
+        if (stickerIdList[idx].equals("1")) {
+          MyItemCard itemCard = new MyItemCard(R.drawable.foo, "",
+                                               userList[idx], false);
+          itemList.add(itemCard);
+          //continue;
+        } else if (stickerIdList[idx].equals("2")) {
+          MyItemCard itemCard = new MyItemCard(R.drawable.thinking_face,
+                                               "", userList[idx], false);
+          itemList.add(itemCard);
+        }
+      }
+      createRecyclerView();
+    }
+  }
+
+  // Add data to firebase button
+  public void doAddDataToDb(View view) {
+    // Write a message to the database
+    DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("message");
+
+    Task t = myRef.setValue("Hello, World!");
+    if (!t.isSuccessful()) {
+      Toast.makeText(getApplicationContext(), "Failed to write value into firebase. ",
+                     Toast.LENGTH_SHORT).show();
+      return;
+    }
+
+    // Read from the database by listening for a change to that item.
+    myRef.addValueEventListener(new ValueEventListener() {
+      @Override
+      public void onDataChange(DataSnapshot dataSnapshot) {
+        // This method is called once with the initial value and again
+        // whenever data at this location is updated.
+        String value = dataSnapshot.getValue(String.class);
+        Log.d(TAG, "Value is: " + value);
+      }
+
+      @Override
+      public void onCancelled(DatabaseError error) {
+        // Failed to read value
+        Log.w(TAG, "Failed to read value.", error.toException());
+        Toast.makeText(getApplicationContext(), "Failed to write value into firebase. ",
+                       Toast.LENGTH_SHORT).show();
+      }
+
+    });
   }
 
   // Reset USERS Button
@@ -248,187 +370,5 @@ public class RealtimeDatabaseActivity extends AppCompatActivity {
     } else if (t1.isSuccessful() && t2.isSuccessful()) {
       Toast.makeText(getApplicationContext(), "Unable to reset player2!", Toast.LENGTH_SHORT).show();
     }
-
-
   }
-
-  // Add data to firebase button
-  public void doAddDataToDb(View view) {
-    // Write a message to the database
-    DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("message");
-
-    Task t = myRef.setValue("Hello, World!");
-    if (!t.isSuccessful()) {
-      Toast.makeText(getApplicationContext()
-              , "Failed to write value into firebase. ", Toast.LENGTH_SHORT).show();
-      return;
-    }
-
-    // Read from the database by listening for a change to that item.
-    myRef.addValueEventListener(new ValueEventListener() {
-      @Override
-      public void onDataChange(DataSnapshot dataSnapshot) {
-        // This method is called once with the initial value and again
-        // whenever data at this location is updated.
-        String value = dataSnapshot.getValue(String.class);
-        Log.d(TAG, "Value is: " + value);
-        //TextView tv = (TextView) findViewById(R.id.dataUpdateTextView);
-        //tv.setText(value);
-        //
-        dataSnapshot.getKey().equals("users");
-        //
-      }
-
-      @Override
-      public void onCancelled(DatabaseError error) {
-        // Failed to read value
-        Log.w(TAG, "Failed to read value.", error.toException());
-        Toast.makeText(getApplicationContext()
-                , "Failed to write value into firebase. ", Toast.LENGTH_SHORT).show();
-      }
-
-    });
-
-  }
-
-
-  /**
-   * Called on score_user1 add
-   *
-   * @param postRef
-   * @param user
-   */
-  private void onAddScore(DatabaseReference postRef, String user, String imageNum,
-                          String senderName) {
-    postRef
-            .child("users")
-            .child(user)
-            .runTransaction(new Transaction.Handler() {
-              @Override
-              public Transaction.Result doTransaction(MutableData mutableData) {
-
-                User user = mutableData.getValue(User.class);
-                if (user == null) {
-                  return Transaction.success(mutableData);
-                }
-
-                //user.score = String.valueOf(Integer.valueOf(user.score) + 5);
-                user.score = user.score + imageNum + ",";
-                //user.senders = user.senders + myUser.username + " ";
-                user.senders = user.senders + senderName + ",";
-                user.scoreTimes = user.scoreTimes + Utils.date() + ",";
-
-                mutableData.setValue(user);
-                int i = 0;
-
-                return Transaction.success(mutableData);
-              }
-
-              @Override
-              public void onComplete(DatabaseError databaseError, boolean b,
-                                     DataSnapshot dataSnapshot) {
-                // Transaction completed
-                Log.d(TAG, "postTransaction:onComplete:" + databaseError);
-                Toast.makeText(getApplicationContext()
-                        , "DBError: " + databaseError, Toast.LENGTH_SHORT).show();
-              }
-
-
-            });
-  }
-
-  private String parseData(String inputStr) throws JSONException {
-    JSONObject jObject = new JSONObject(inputStr);
-    Iterator<?> keys = jObject.keys();
-    while (keys.hasNext()) {
-      String key = (String) keys.next();
-      if (key.equals(USER_1)) {
-        return jObject.getString(key);
-      }
-    }
-    return "Nothing matched!";
-
-  }
-
-  public void showReadData(View view, String addition) {
-    mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
-      @Override
-      public void onDataChange(DataSnapshot dataSnapshot) {
-        //Log.i("status:","INSIDEEEEE");
-        String curUsername = myUser.username;
-
-        String[] stickerIdList = getStringList(String.valueOf(dataSnapshot.child("users")
-                                                                      .child(curUsername + addition).child("score").getValue()));
-        String[] userList = getStringList(String.valueOf(dataSnapshot.child("users")
-                                                                 .child(curUsername + addition).child("senders").getValue()));
-        String[] msgTimeList = getStringList(String.valueOf(dataSnapshot.child("users")
-                                                                    .child(curUsername + addition).child("scoreTimes").getValue()));
-
-
-//                String [] stickerIdList = getStringList(String.valueOf(dataSnapshot.child
-//                ("users").child("user1" + addition).child("score").getValue()));
-//                String [] userList = getStringList(String.valueOf(dataSnapshot.child("users")
-//                .child("user1" + addition).child("senders").getValue()));
-        itemList = new ArrayList<>();
-        for (int idx = 0; idx < stickerIdList.length; idx++) {
-          if (stickerIdList[idx].equals("1")) {
-            MyItemCard itemCard = new MyItemCard(R.drawable.foo,
-                                                 userList[idx], msgTimeList[idx], false);
-            itemList.add(itemCard);
-            //continue;
-          } else if (stickerIdList[idx].equals("2")) {
-            MyItemCard itemCard = new MyItemCard(R.drawable.thinking_face, "",
-                                                 userList[idx], false);
-            itemList.add(itemCard);
-          }
-        }
-        createRecyclerView();
-
-
-      }
-
-      @Override
-      public void onCancelled(DatabaseError databaseError) {
-        // ...
-      }
-    });
-  }
-
-  private String[] getStringList(String inputString) {
-    return inputString.split(",");
-  }
-
-
-  private void drawEmotes(DataSnapshot dataSnapshot) {
-    User user = dataSnapshot.getValue(User.class);
-
-    String whoseInfo = player.isChecked() ? myUser.username : myUser.username + "sent";
-    Log.i("INFO", whoseInfo);
-
-    if (dataSnapshot.getKey().equalsIgnoreCase(whoseInfo)) {
-      //score_user1.setText(String.valueOf(user.score));
-      //user1.setText(user.username);
-      String[] stickerIdList = getStringList(String.valueOf(user.score));
-      String[] userList = getStringList(String.valueOf(user.senders));
-      itemList = new ArrayList<>();
-      for (int idx = 0; idx < stickerIdList.length; idx++) {
-        if (stickerIdList[idx].equals("1")) {
-          MyItemCard itemCard = new MyItemCard(R.drawable.foo, "",
-                                               userList[idx], false);
-          itemList.add(itemCard);
-          //continue;
-        } else if (stickerIdList[idx].equals("2")) {
-          MyItemCard itemCard = new MyItemCard(R.drawable.thinking_face,
-                                               "", userList[idx], false);
-          itemList.add(itemCard);
-        }
-      }
-      createRecyclerView();
-    } else {
-      //score_user2.setText(String.valueOf(user.score));
-      //user2.setText(user.username);
-    }
-  }
-
-
 }
